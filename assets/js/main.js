4e7f59b95ac228728cb181136578fd49ca619909
Gustavo <gusegonzalez97@gmail.com>
2021-04-09 19:50:25 -0300

    ain

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,26 @@
 /*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu')
+const toggleMenu = document.getElementById('nav-toggle')
+const closeMenu=document.getElementById('nav-close')
 
 
 // SHOW
+toggleMenu.addEventListener('click',()=>{
+    navMenu.classList.toggle('show')
+})
 
+closeMenu.addEventListener('click',()=>{
+    navMenu.classList.remove('show')
+})
 
-// HIDDEN
+const navLink =document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+const sections = document.querySelectorAll('section[id]')
 
 
 /*===== ACTIVE AND REMOVE MENU =====*/
